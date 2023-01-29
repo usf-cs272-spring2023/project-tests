f57e0aa49178a6615c2f9812116377ce5a27e002
Sophie Engle <sjengle@cs.usfca.edu>
2023-01-28 18:08:15 -0800

    Simplifying

--- src/test/java/edu/usfca/cs272/BuildIndexTests.java
+++ src/test/java/edu/usfca/cs272/BuildIndexTests.java
@@ -1,12 +1,20 @@
 package edu.usfca.cs272;
 
+import static edu.usfca.cs272.ProjectFlag.COUNTS;
 import static edu.usfca.cs272.ProjectFlag.INDEX;
 import static edu.usfca.cs272.ProjectFlag.TEXT;
 import static edu.usfca.cs272.ProjectPath.ACTUAL;
 import static edu.usfca.cs272.ProjectPath.EXPECTED;
+import static edu.usfca.cs272.ProjectPath.HELLO;
 import static edu.usfca.cs272.ProjectTests.LONG_TIMEOUT;
+import static edu.usfca.cs272.ProjectTests.SHORT_TIMEOUT;
+import static edu.usfca.cs272.ProjectTests.testNoExceptions;
+import static org.junit.jupiter.params.provider.EnumSource.Mode.MATCH_ALL;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.ClassOrderer;
@@ -22,135 +30,162 @@ import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 /**
- * A test suite for project v1.1. During development, run individual tests
- * instead of this entire test suite!
+ * A test suite for project v1.1.
  *
  * @author CS 272 Software Development (University of San Francisco)
  * @version Spring 2023
  */
 @TestClassOrder(ClassOrderer.OrderAnnotation.class)
-public class IndexTests {
+public class BuildIndexTests {
 	/**
-	 * Tests the index output of this project.
+	 * Tests the output of this project.
 	 */
 	@Nested
 	@Order(1)
 	@TestMethodOrder(OrderAnnotation.class)
 	public class FileTests {
 		/**
-		 * Tests the index output for files in the simple subdirectory.
+		 * See the JUnit output for test details.
 		 *
-		 * @param filename filename of a text file in the simple subdirectory
+		 * @param filename the filename
 		 */
 		@ParameterizedTest
 		@Order(1)
 		@ValueSource(strings = { "hello.txt", "animals.text", "capitals.txt", "capital_extension.TXT", "digits.tXt",
 				"position.teXt", "empty.txt", "symbols.txt", "words.tExT" })
-		public void testSimpleFiles(String filename) {
+		public void testSimple(String filename) {
 			Path input = ProjectPath.SIMPLE.resolve(filename);
-			testIndexOutput("simple", input, ProjectPath.id(input));
+			testOutput("simple", input, ProjectPath.id(input));
 		}
 
 		/**
-		 * Tests the index output for files in the simple subdirectory.
+		 * See the JUnit output for test details.
 		 *
-		 * @param filename filename of a non-text file in the simple subdirectory
+		 * @param filename the filename
 		 */
 		@ParameterizedTest
 		@Order(2)
 		@ValueSource(strings = { "no_extension", "sentences.md", "double_extension.txt.html", "wrong_extension.html" })
-		public void testOtherFiles(String filename) {
+		public void testOther(String filename) {
 			Path input = ProjectPath.SIMPLE.resolve(filename);
-			testIndexOutput("simple", input, ProjectPath.id(input));
+			testOutput("simple", input, ProjectPath.id(input));
 		}
 
 		/**
-		 * Tests the index output for files in the stems subdirectory.
+		 * See the JUnit output for test details.
 		 *
-		 * @param path the project path to use
+		 * @param path the path
 		 */
 		@ParameterizedTest
 		@Order(3)
 		@EnumSource(names = { "STEMS_IN", "STEMS_OUT" })
-		public void testStemsFiles(ProjectPath path) {
-			testIndexOutput("stems", path);
+		public void testStems(ProjectPath path) {
+			testOutput("stems", path);
 		}
 
 		/**
-		 * Tests the index output for files in the RFCs subdirectory.
+		 * See the JUnit output for test details.
 		 *
-		 * @param path the project path to use
+		 * @param path the path
 		 */
 		@ParameterizedTest
 		@Order(4)
-		@EnumSource(mode = EnumSource.Mode.MATCH_ALL, names = "^RFCS_.+")
-		public void testRfcsFiles(ProjectPath path) {
-			testIndexOutput("rfcs", path);
+		@EnumSource(mode = MATCH_ALL, names = "^RFCS_.+")
+		public void testRfcs(ProjectPath path) {
+			testOutput("rfcs", path);
 		}
 
 		/**
-		 * Tests the index output for files in the guten subdirectory.
+		 * See the JUnit output for test details.
 		 *
-		 * @param path the project path to use
+		 * @param path the path
 		 */
 		@ParameterizedTest
 		@Order(5)
-		@EnumSource(mode = EnumSource.Mode.MATCH_ALL, names = "^GUTEN_.+")
-		public void testGutenFiles(ProjectPath path) {
-			testIndexOutput("guten", path);
+		@EnumSource(mode = MATCH_ALL, names = "^GUTEN_.+")
+		public void testGuten(ProjectPath path) {
+			testOutput("guten", path);
 		}
 	}
 
 	/**
-	 * Tests the index output of this project.
+	 * Tests the output of this project.
 	 */
 	@Nested
 	@Order(2)
 	@TestMethodOrder(OrderAnnotation.class)
 	public class DirectoryTests {
 		/**
-		 * Tests the index output for the simple subdirectory.
+		 * See the JUnit output for test details.
 		 */
 		@Test
 		@Order(1)
-		public void testSimpleDirectory() {
-			testIndexOutput("simple", ProjectPath.SIMPLE);
+		public void testSimple() {
+			testOutput("simple", ProjectPath.SIMPLE);
 		}
 
 		/**
-		 * Tests the index output for the stems subdirectory.
+		 * See the JUnit output for test details.
 		 */
 		@Test
 		@Order(2)
-		public void testStemsDirectory() {
-			testIndexOutput("stems", ProjectPath.STEMS);
+		public void testStems() {
+			testOutput("stems", ProjectPath.STEMS);
 		}
 
 		/**
-		 * Tests the index output for the rfcs subdirectory.
+		 * See the JUnit output for test details.
 		 */
 		@Test
 		@Order(3)
-		public void testRfcDirectory() {
-			testIndexOutput("rfcs", ProjectPath.RFCS);
+		public void testRfcs() {
+			testOutput("rfcs", ProjectPath.RFCS);
 		}
 
 		/**
-		 * Tests the index output for the guten subdirectory.
+		 * See the JUnit output for test details.
 		 */
 		@Test
 		@Order(4)
-		public void testGutenDirectory() {
-			testIndexOutput("guten", ProjectPath.GUTEN);
+		public void testGuten() {
+			testOutput("guten", ProjectPath.GUTEN);
 		}
 
 		/**
-		 * Tests the index output for all of the text files.
+		 * See the JUnit output for test details.
 		 */
 		@Test
 		@Order(5)
 		public void testText() {
-			testIndexOutput(".", ProjectPath.TEXT);
+			testOutput(".", ProjectPath.TEXT);
+		}
+
+		/**
+		 * See the JUnit output for test details.
+		 */
+		@Test
+		@Order(6)
+		public void testCountsIndex() {
+			ProjectPath input = ProjectPath.TEXT;
+			String indexName = String.format("index-%s.json", input.id);
+			String countsName = String.format("counts-%s.json", input.id);
+
+			Path indexActual = ACTUAL.resolve(indexName);
+			Path countsActual = ACTUAL.resolve(countsName);
+
+			String[] args = {
+					TEXT.flag, input.text,
+					INDEX.flag, indexActual.toString(),
+					COUNTS.flag, countsActual.toString()
+			};
+
+			Map<Path, Path> files = Map.of(
+					ACTUAL.resolve(countsName), EXPECTED.resolve("counts").resolve(countsName),
+					ACTUAL.resolve(indexName), EXPECTED.resolve("index").resolve(indexName)
+			);
+
+			Executable test = () -> ProjectTests.checkOutput(args, files);
+			Assertions.assertTimeoutPreemptively(LONG_TIMEOUT, test);
 		}
 	}
 
@@ -161,37 +196,115 @@ public class IndexTests {
 	@Order(3)
 	@TestMethodOrder(OrderAnnotation.class)
 	public class ExceptionTests {
+		/**
+		 * Tests no exceptions are thrown with the provided arguments.
+		 *
+		 * @throws IOException if IO error occurs
+		 */
+		@Test
+		@Order(1)
+		public void testDefaultIndex() throws IOException {
+			String[] args = { TEXT.flag, HELLO.text, INDEX.flag };
 
+			Files.deleteIfExists(COUNTS.path);
+			Files.deleteIfExists(INDEX.path);
+			testNoExceptions(args, SHORT_TIMEOUT);
+
+			Assertions.assertAll(
+					() -> Assertions.assertTrue(Files.exists(INDEX.path)),
+					() -> Assertions.assertFalse(Files.exists(COUNTS.path))
+			);
+		}
+
+		/**
+		 * Tests no exceptions are thrown with the provided arguments.
+		 *
+		 * @throws IOException if IO error occurs
+		 */
+		@Test
+		@Order(2)
+		public void testOnlyIndex() throws IOException {
+			String[] args = { INDEX.flag };
+			Files.deleteIfExists(INDEX.path);
+			testNoExceptions(args, SHORT_TIMEOUT);
+			Assertions.assertTrue(Files.exists(INDEX.path));
+		}
+
+		/**
+		 * Tests no exceptions are thrown with the provided arguments.
+		 *
+		 * @throws IOException if IO error occurs
+		 */
+		@Order(3)
+		@Test
+		public void testSwitched() throws IOException {
+			String[] args = { INDEX.flag, TEXT.flag, HELLO.text };
+			Files.deleteIfExists(INDEX.path);
+			testNoExceptions(args, SHORT_TIMEOUT);
+			Assertions.assertTrue(Files.exists(INDEX.path));
+		}
+
+		/**
+		 * Tests no exceptions are thrown with the provided arguments.
+		 *
+		 * @throws IOException if IO error occurs
+		 */
+		@Test
+		@Order(1)
+		public void testNoCounts() throws IOException {
+			String[] args = { TEXT.flag, HELLO.text, INDEX.flag };
+			Files.deleteIfExists(COUNTS.path);
+			testNoExceptions(args, SHORT_TIMEOUT);
+			Assertions.assertFalse(Files.exists(COUNTS.path));
+		}
+
+		/**
+		 * Tests no exceptions are thrown with the provided arguments.
+		 *
+		 * @throws IOException if IO error occurs
+		 */
+		@Test
+		@Order(6)
+		public void testNoOutput() throws IOException {
+			String[] args = { TEXT.flag, HELLO.text };
+
+			Files.deleteIfExists(COUNTS.path);
+			Files.deleteIfExists(INDEX.path);
+			testNoExceptions(args, SHORT_TIMEOUT);
+
+			Assertions.assertAll(
+					() -> Assertions.assertFalse(Files.exists(COUNTS.path)),
+					() -> Assertions.assertFalse(Files.exists(INDEX.path))
+			);
+			;
+		}
 	}
 
 	/**
 	 * Generates the arguments to use for the output test cases. Designed to be used
 	 * inside a JUnit test.
 	 *
-	 * @param subdir the output subdirectory to use
-	 * @param input the input path to use
-	 * @param id the id to use in the output filename
+	 * @param subdir the subdir
+	 * @param input the input
+	 * @param id the id
 	 */
-	public static void testIndexOutput(String subdir, Path input, String id) {
+	public static void testOutput(String subdir, Path input, String id) {
 		String filename = String.format("index-%s.json", id);
 		Path actual = ACTUAL.resolve(filename);
 		Path expected = EXPECTED.resolve("index").resolve(subdir).resolve(filename);
-
 		String[] args = { TEXT.flag, input.normalize().toString(), INDEX.flag, actual.normalize().toString() };
-
 		Executable test = () -> ProjectTests.checkOutput(args, actual, expected);
 		Assertions.assertTimeoutPreemptively(LONG_TIMEOUT, test);
 	}
 
 	/**
-	 * Calls {@link IndexTests#testIndexOutput(String, Path, String)} using the
-	 * enum.
+	 * Calls {@link BuildIndexTests#testOutput(String, Path, String)} using the enum.
 	 *
-	 * @param subdir the output subdirectory to use
-	 * @param path the project path to use
-	 * @see CountTests#testCountOutput(Path, String)
+	 * @param subdir the subdir
+	 * @param path the path
+	 * @see BuildCountTests#testOutput(Path, String)
 	 */
-	public static void testIndexOutput(String subdir, ProjectPath path) {
-		testIndexOutput(subdir, path.path, path.id);
+	public static void testOutput(String subdir, ProjectPath path) {
+		testOutput(subdir, path.path, path.id);
 	}
 }
