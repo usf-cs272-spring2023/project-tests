4cec7f8e66d3b4e7ff8b33036f81dc5d5e0e45a1
Sophie Engle <sjengle@cs.usfca.edu>
2023-04-19 15:11:48 -0700

    Adding test placeholders.

---
+++ src/test/java/edu/usfca/cs272/CrawlSiteTests.java
@@ -0,0 +1,16 @@
+package edu.usfca.cs272;
+
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.TestClassOrder;
+
+/**
+ * A test suite for project v4.0.
+ *
+ * @author CS 272 Software Development (University of San Francisco)
+ * @version Spring 2023
+ */
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+public class CrawlSiteTests {
+
+
+}
